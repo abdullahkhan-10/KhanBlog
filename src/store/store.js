1c254bdb7f841b9e02a553b6a53737dd136ddd13
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2025-02-09 11:11:40 +0500

    Reducer add to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "../store/authSlice"
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authSliceReducer
+    }
 })
 
 export default store;
