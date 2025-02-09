0b917abcc10349a7144a807b9fb4de63b4ff41eb
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2025-02-09 11:04:46 +0500

    Redux toolkit store configuration added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
+import { useState, useEffect } from "react"
 import AllBlogs from "./components/AllBlogs"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import { useDispatch } from "react-redux"
+import authService from "./auth/auth"
+import { login, logout } from "./store/authSlice"
+
 
 function App() {
   // console.log(import.meta.env.VITE_API_KEY );
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect( ()=>{
+    authService.getCurrentUser()
+    .then( (userInfo) =>{
+      if (userInfo) {
+        dispatch(login(userInfo))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally( ()=>setLoading(true))
+  },[])
   
-  return (
+  return loading ? (
     <div className="bg-white">
       <Header/>
-      <AllBlogs/>
+      <main>
+       <AllBlogs/>
+      </main>
       <Footer/>
     </div>
-  )
+  ) : null
 }
 
 export default App
